b8688b81bb6fbdf5fb01a624d17066ddf2f588e6
RabinThapa1998 <imrabin1998@gmail.com>
2023-02-10 22:11:44 +0545

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StatusBar } from "expo-status-bar";
+import { Text, View, SafeAreaView, Button } from "react-native";
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="border border-gray-300">
+      <View className="px-3 py-5">
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Text>Hello World</Text>
+        <Text className="text-blue-500 text-xl ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum
+          dicta nihil unde eos enim iste reiciendis temporibus officia quae
+          consequuntur, aliquid quos recusandae ipsa aperiam, similique nisi
+          quasi nobis repellendus fuga optio. Accusamus?
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum
+          dicta nihil unde eos enim iste reiciendis temporibus officia quae
+          consequuntur, aliquid quos recusandae ipsa aperiam, similique nisi
+          quasi nobis repellendus fuga optio. Accusamus?
+        </Text>
+        <Button title="Click Me" className="bg-orange-500 w-1/2 border" />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
