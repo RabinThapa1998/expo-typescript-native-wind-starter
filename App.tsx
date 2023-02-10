2dc24b718926dcb428e29f654de0b9f20efbc786
RabinThapa1998 <imrabin1998@gmail.com>
2023-02-10 23:26:39 +0545

    fix:lints

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View, SafeAreaView, Button } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, SafeAreaView, Button } from 'react-native';
+
 function App() {
   const handleButtonClick = () => {
-    console.log("Button Clicked");
+    console.log('Button Clicked');
   };
   return (
     <SafeAreaView className="border border-gray-300">
@@ -10,19 +11,17 @@ function App() {
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
         <Text>Hello World</Text>
-        <Text className="text-blue-500 text-xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum
-          dicta nihil unde eos enim iste reiciendis temporibus officia quae
-          consequuntur, aliquid quos recusandae ipsa aperiam, similique nisi
-          quasi nobis repellendus fuga optio. Accusamus?
+        <Text className="text-blue-500 text-xl px-5 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum dicta nihil unde eos
+          enim iste reiciendis temporibus officia quae consequuntur, aliquid quos recusandae ipsa
+          aperiam, similique nisi quasi nobis repellendus fuga optio. Accusamus?
         </Text>
         <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum
-          dicta nihil unde eos enim iste reiciendis temporibus officia quae
-          consequuntur, aliquid quos recusandae ipsa aperiam, similique nisi
-          quasi nobis repellendus fuga optio. Accusamus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum dicta nihil unde eos
+          enim iste reiciendis temporibus officia quae consequuntur, aliquid quos recusandae ipsa
+          aperiam, similique nisi quasi nobis repellendus fuga optio. Accusamus?
         </Text>
-        <Button title="Click Me" color={"green"} onPress={handleButtonClick} />
+        <Button title="Click Me" color={'green'} onPress={handleButtonClick} />
       </View>
     </SafeAreaView>
   );
